b0d83aaf7d7aa34214ea67be2a04c3ba815b6e7a
T. Bunratta <tharathorn.bu@ku.th>
2020-09-22 15:16:08 +0700

    Add JDBC database link

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.mindrot.jbcrypt.BCrypt;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
@@ -11,21 +13,25 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
+    private CustomerRepository repository;
 
-    @PostConstruct
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+   /* @PostConstruct
     public void postConstruct() {
         this.customerList = new ArrayList<>();
-    }
+    }*/
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     private String hash(String pin) {
@@ -33,11 +39,11 @@ public class CustomerService {
         return BCrypt.hashpw(pin, salt);
     }
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
     }
     public Customer checkPin(Customer inputCustomer) {
         // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
